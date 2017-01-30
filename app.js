359dd82ab9f7603fe3e286e608d7cafcd9c97c5e
Pedro Azevedo <pedrocesar.ti@gmail.com>
2017-01-30 21:38:35 +0000

    Fixing typo.

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ app.set('port', (process.env.PORT || 5000))
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
-
 app.get('/', function (req, res) {
     res.send('Hi, I am a bot!')
 })
@@ -38,7 +37,7 @@ app.post('/webhook/', function (req, res) {
                 sendTextMessage(sender, "Turning on the light 💡💡")
         	rpio.write(12, rpio.HIGH);
             }
-            else if(text == "turnoffr"){
+            else if(text == "turnoff"){
         	rpio.write(12, rpio.LOW);
                 sendTextMessage(sender, "Turning off the lights 🔌")
 	    }
